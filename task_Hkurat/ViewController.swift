225821363d3cb04e9250ce12c6a0cefda3b62247
youssef <youssef.esmaileffekygmail.com@youssefs-mac.local>
2019-03-23 15:10:42 +0200

    Initial Commit

---
+++ task_Hkurat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task_Hkurat
+//
+//  Created by youssef on 3/23/19.
+//  Copyright © 2019 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
